708d23d4ec60669718ddeba758e354deb0652410
Frencil <chris.clark@sparkfun.com>
2015-04-14 15:55:22 -0600

    more complete labels for both elements and nuclides

--- images/elements/spritegen.py
+++ images/elements/spritegen.py
@@ -15,7 +15,7 @@ jpgs.insert(0, jpgs.pop())
 
 # Max protons - largest numbered image, necessary for setting sprite map size
 max_protons = int(re.findall(r"\d+",jpgs[len(jpgs)-1])[0])
-print("[SPRITEGEN] Found " + str(len(jpgs)) + " jpgs, max protons: " + str(max_protons))
+print("Found " + str(len(jpgs)) + " jpgs, max protons: " + str(max_protons))
 
 # Scaling for each thumb in the sprite (multiplier on minimum size of 52x29)
 scale = 4
@@ -24,7 +24,7 @@ scale = 4
 map_width = base_w * scale
 map_height = (base_h * scale) * (max_protons + 1)
 sprite_map = Image.new('RGBA', (map_width, map_height), (0, 0, 0, 0))
-print("[SPRITEGEN] Initialized sprite map: " + str(map_width) + "x" + str(map_height))
+print("Initialized sprite map: " + str(map_width) + "x" + str(map_height))
 
 # Walk the list of jpgs to scale and add each to the map
 for jpg_path in jpgs:
@@ -38,11 +38,11 @@ for jpg_path in jpgs:
         protons = int(re.findall(r"\d+", jpg_path)[0])
         offset = 0, protons * base_h * scale
 
-    print("[SPRITEGEN] Pasting " + jpg_path)
+    print("Pasting " + jpg_path)
     sprite_map.paste(jpg, offset)
 
-# Save the map as a PNG. Done!
-print("[SPRITEGEN] Writing final sprite map")
+# Save the map as a PNG.
+print("Writing final sprite map")
 sprite_map.save("sprite_map.png")
 
-print("[SPRITEGEN] Done.")
+print("Done.")
